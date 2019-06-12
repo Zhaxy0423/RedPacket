bf114a47912626d524e0bc7969d939b4bd1e3a43
zhaxy0423 <Zhaxy0423@163.com>
2019-06-12 18:39:26 +0800

    抢红包基本实现v1.0

--- src/main/java/com/xkj/redpacket/control/StationController.java
+++ src/main/java/com/xkj/redpacket/control/StationController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @author 渣小宇 
+ * @author 渣小宇
  */
 @Controller
 public class StationController {
